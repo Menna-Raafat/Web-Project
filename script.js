16e7e61d9e33d0842c81505921dc772b1c29cfbb
Menna-Raafat <mennaraafat89@gmail.com>
2024-12-12 23:23:47 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.course-card button').forEach(button => {
+    button.addEventListener('click', () => {
+      alert('Course details will be available soon!');
+    });
+  });
+  
